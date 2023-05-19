f39ff11ed1a926ff75a2b4c8976a96f6df25bf0c
MuhSaktiR <seken876@gmail.com>
2023-05-19 15:27:39 +0700

    membuat tipe data int dan char dan cout dan cin dalam void list::addNode

--- singlylingkedlist/singlylingkedlist.cpp
+++ singlylingkedlist/singlylingkedlist.cpp
@@ -29,5 +29,10 @@ List::List()
 
 void List::addNode() /*Menambah sebuah Node kedalam list*/
 {
-
+	int nim;
+	char nm[20];
+	cout << "\nMasukkan Nomor Mahasiswa: ";
+	cin >> nim;
+	cout << "\nMasukkan Nama: ";
+	cin >> nm;
 }
